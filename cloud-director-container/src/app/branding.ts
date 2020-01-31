724b9d2b892cba8af7e126c165897602a782964d
Jeff Moroski <jmoroski@vmware.com>
2020-01-31 16:04:08 -0500

    Replace main app HTML with VCD-like UI container.
    This change introduces a header and main content pane that is a
    stylistically similar (though probably not pixel perfect) recreation
    of the main UI layout in Cloud Director 9.7 and 10.0.
    
    A simple branding object has also been exposed, along with a provider to
    associate it with environment-specific data.
    
    Testing Done:
    - Ran `ng serve` and verified the new look of the main page
    - Changed headerTitle and verified live reload of UI with new header
    - Added test png to assets, modified environment and verified successful
      load of image asset for branding logo
    
    Signed-off-by: Jeff Moroski <jmoroski@vmware.com>

---
+++ cloud-director-container/src/app/branding.ts
@@ -0,0 +1,8 @@
+import { InjectionToken } from "@angular/core";
+
+export class Branding {
+    headerTitle: string;
+    headerLogo?: string;
+}
+
+export const CONTAINER_BRANDING = new InjectionToken<Branding>("White labaling data");
