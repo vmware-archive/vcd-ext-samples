fd27cc790a884b387300c97727564a89ef16764f
Jeff Moroski <jmoroski@vmware.com>
2020-02-10 08:32:38 -0500

    Move branding under environments/ directory.
    Signed-off-by: Jeff Moroski <jmoroski@vmware.com>

--- cloud-director-container/src/environments/environment.ts
+++ cloud-director-container/src/environments/environment.ts
@@ -1,4 +1,4 @@
-import { Branding } from "src/app/branding";
+import { Branding } from "./branding";
 
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
