310a38ba662a584728196cc6a36f76e2bf9cfc51
Jeff Moroski <jmoroski@vmware.com>
2020-02-11 14:26:46 -0500

    Give main app component access to plugin registrations.
    Signed-off-by: Jeff Moroski <jmoroski@vmware.com>

--- cloud-director-container/src/app/app.component.ts
+++ cloud-director-container/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { CONTAINER_BRANDING, Branding } from '../environments/branding';
 import { Title } from '@angular/platform-browser';
+import { PluginRegistration, PLUGINS } from 'src/plugins';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,14 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  constructor(@Inject(CONTAINER_BRANDING) private branding: Branding, private title: Title) {}
+  private _pluginRegistrations: PluginRegistration[] = [];
+  get pluginRegistrations(): PluginRegistration[] {
+    return this._pluginRegistrations;
+  }
+
+  constructor(@Inject(CONTAINER_BRANDING) private branding: Branding, private title: Title) {
+    this._pluginRegistrations = PLUGINS;
+  }
 
   ngOnInit(): void {
     this.title.setTitle(this.branding.headerTitle);
